08770118f322a454a1743bae90ed250d4ff62e19
afnanshahid505 <afnanshahid744@gmail.com>
2025-09-20 20:09:18 +0530

    login

--- app/src/main/java/com/example/gen69/MainActivity.java
+++ app/src/main/java/com/example/gen69/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gen69;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.loginButton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"button clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
